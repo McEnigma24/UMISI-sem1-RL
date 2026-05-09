da7165a384849bbd355965f87684983f392305c8
McEnigma24 <116126603+McEnigma24@users.noreply.github.com>
2026-05-09 13:58:38 +0200

    asdf

--- l4/start.ps1
+++ l4/start.ps1
@@ -6,15 +6,15 @@ cls
 
 
 $env:KERAS_BACKEND = "torch"
-# python solution.py --env cartpole
+python solution.py --env cartpole
 # python solution.py --env cartpole --resume cartpole_checkpoint_ep1100.keras
 # python solution.py --env cartpole --resume cartpole_separete_networks_2\cartpole_checkpoint_ep1600.keras
 
 
 # Critic-Check #
-python solution.py --env cartpole --check-critic cartpole_single_network\cartpole_final.keras
-python solution.py --env cartpole --check-critic cartpole_separate_networks_1\cartpole_final.keras
-python solution.py --env cartpole --check-critic cartpole_separate_networks_2\cartpole_final.keras
-python solution.py --env cartpole --check-critic cartpole_separate_networks_3\cartpole_final.keras
+# python solution.py --env cartpole --check-critic cartpole_single_network\cartpole_final.keras
+# python solution.py --env cartpole --check-critic cartpole_separate_networks_1\cartpole_final.keras
+# python solution.py --env cartpole --check-critic cartpole_separate_networks_2\cartpole_final.keras
+# python solution.py --env cartpole --check-critic cartpole_separate_networks_3\cartpole_final.keras
 
 # py -3.13
